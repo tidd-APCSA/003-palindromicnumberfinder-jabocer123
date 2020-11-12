b24fc0223a5b052d008ba0e1ca05b8b62e29ffbd
jabocer123 <tjzotte@outlook.com>
2020-11-12 15:55:00 +0000

    Maybe Finished

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -13,21 +13,37 @@ public class PalindromicNumberFinder {
 
     // this method should find the next palindromic number
     public int searchForPalindromicNum(int num){
-
+      
+      int nextPal = num + 1;
+      
+      while(testPalindromicNum(nextPal) == false){
+        nextPal++;
+      }
+      return nextPal;
     }
 
     // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
     public boolean testPalindromicNum(int num){
+      boolean isPalindromic = false;
 
-
+      while(isPalindromic == false){
+        if((Integer.toString(num)).equals(reverseNum(Integer.toString(num))) == true){
+          isPalindromic = true;
+        }
+      }
+      return isPalindromic;
     }
 
 
     // this is a helper method for testPalindromicNum. It should reverse the number and return it.
-    public String reverseNum(int num){
-
-
-
+    public String reverseNum(String num){
+      
+      String str = "";
+      for(int i = 0; i < num.length(); i++){
+        str = str + num.charAt(num.length() - (1 + i));
+      }
+      
+      return str;
     }
 
 
--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -1,6 +1,8 @@
 public class PalindromicRunner {
     public static void main(String[] args) {
         // be sure you're testing each method as you work then produce your final output using the toString method
+      PalindromicNumberFinder first = new PalindromicNumberFinder(72927);
+      System.out.println(first.toString());
 
     }
 }
