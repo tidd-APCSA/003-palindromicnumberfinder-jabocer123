bf44021d61a021c39493c957907e12126a6dc2db
jabocer123 <tjzotte@outlook.com>
2020-11-12 16:11:37 +0000

    More likely Finished

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -13,34 +13,31 @@ public class PalindromicNumberFinder {
 
     // this method should find the next palindromic number
     public int searchForPalindromicNum(int num){
-      
+      int firstNum = num;
       int nextPal = num + 1;
       
       while(testPalindromicNum(nextPal) == false){
         nextPal++;
       }
-      return nextPal;
+      return nextPal - firstNum;
     }
 
     // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
     public boolean testPalindromicNum(int num){
-      boolean isPalindromic = false;
-
-      while(isPalindromic == false){
-        if((Integer.toString(num)).equals(reverseNum(Integer.toString(num))) == true){
-          isPalindromic = true;
-        }
-      }
-      return isPalindromic;
+      Integer number = num;
+      String reversedNum = reverseNum(num);
+      String firstNum = number.toString();
+      return firstNum.equals(reversedNum);
     }
 
 
     // this is a helper method for testPalindromicNum. It should reverse the number and return it.
-    public String reverseNum(String num){
-      
+    public String reverseNum(int num){
+      Integer thisNum = num;
+      String s = thisNum.toString();
       String str = "";
-      for(int i = 0; i < num.length(); i++){
-        str = str + num.charAt(num.length() - (1 + i));
+      for(int i = 0; i < s.length(); i++){
+        str = str + s.charAt(s.length() - (1 + i));
       }
       
       return str;
